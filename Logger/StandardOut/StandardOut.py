635d413e3f2aef14c2a6e1fab7ec411e79a4dc83
Ghostkeeper <ghost_keeper+github@hotmail.com>
2016-02-06 15:17:24 +0100

    Enable colour-coded logs in StdOut
    Logs are now coded by colour based on their level, on both Bash and
    Terminal. Not tested yet how this works when piping to a file on
    different machines.

--- Logger/StandardOut/StandardOut.py
+++ Logger/StandardOut/StandardOut.py
@@ -22,14 +22,19 @@
 #
 #For more information, please refer to <https://unlicense.org/>
 
-from Luna.Logger import Logger
+from Luna.Logger import Logger,Level
 import datetime #For putting timestamps alongside each message.
+import ctypes #For printing in colour on Windows machines.
 
 #Logs messages to the standard output of the program.
 class StandardOut(Logger):
 	#Creates a new instance of the StandardOut logger.
 	def __init__(self):
 		Logger.__init__(self)
+		self.__standardOutHandle = None
+		if ctypes and ctypes.windll and ctypes.windll.kernel32: #Windows bash.
+			self.__standardOutHandle = ctypes.windll.kernel32.GetStdHandle(-11) #-11 is the flag for standard output in the Windows API.
+			self.__defaultConsoleAttributes = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(-11)
 
 	#Logs a new message.
 	#
@@ -37,4 +42,34 @@ class StandardOut(Logger):
 	def log(self,level,message,*arguments):
 		formatted = datetime.datetime.strftime(datetime.datetime.now(),"[%H:%M:%S] ") #Format the date and time.
 		formatted += message % arguments #Replace the %'s in the message with the arguments.
-		print(formatted)
+		if ctypes and ctypes.windll and ctypes.windll.kernel32: #Windows bash.
+			self.colourPrintWin32(formatted,level)
+		else:
+			self.colourPrintAnsi(formatted,level)
+
+	def colourPrintWin32(self,message,level):
+		if level == Level.ERROR:
+			ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,12) #Red.
+		elif level == Level.CRITICAL:
+			ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,13) #Magenta.
+		elif level == Level.WARNING:
+			ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,14) #Yellow.
+		elif level == Level.INFO:
+			ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,10) #Green.
+		elif level == Level.DEBUG:
+			ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,9) #Blue.
+		print(message)
+		ctypes.windll.kernel32.SetConsoleTextAttribute(self.__standardOutHandle,15) #White.
+
+	def colourPrintAnsi(self,message,level):
+		if level == Level.ERROR:
+			ansiColour = '\033[38m' #Red.
+		elif level == Level.CRITICAL:
+			ansiColour = '\033[35m' #Magenta.
+		elif level == Level.WARNING:
+			ansiColour = '\033[33m' #Yellow.
+		elif level == Level.INFO:
+			ansiColour = '\033[32m' #Green.
+		elif level == Level.DEBUG:
+			ansiColour = '\033[34m' #Blue.
+		print(ansiColour + message + '\033[m') #Start code, then message, then revert to default colour.
