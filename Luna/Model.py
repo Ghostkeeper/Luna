1515b8c2c34d3f4821ac182ca7a9527de73d76e8
Ghostkeeper <ghost_keeper+github@hotmail.com>
2016-03-12 21:22:03 +0100

    Model as class decorator
    Rather than having function decorators for listening to when functions
    are called, the model is now more explicitly a "model" in that other
    functions can listen to when the value of a member is changed. It isn't
    very neat yet; in particular, the listeners are now strongly linked,
    which keeps their instances from being garbage collected...

--- Luna/Model.py
+++ Luna/Model.py
@@ -32,35 +32,65 @@ For a safe model system, every model should follow the following rules:
 """
 
 from functools import wraps #To retain the documentation and name of the wrapped functions by these decorators.
+from inspect import getargspec #Getting the number of arguments of a function, and setting up listeners for every member.
 from weakref import WeakKeyDictionary,WeakSet #To automatically remove listeners and signallers if their class instances are removed.
 
-__listeners = WeakKeyDictionary()
-"""
-For each function, a set of functions that need to be called whenever the method
-is called.
+from Luna.Logger import warning
 
-Entries of the dictionary will automatically get garbage collected once all
-strong references to their class instances are removed. Entries of each listener
-set will automatically get garbage collected once all strong references to their
-class instances are removed.
-"""
-
-def signal(setter):
+def model(originalClass):
 	"""
-	.. function:: signal(setter)
-	Decorator indicating that a function can be registered with listeners.
+	.. function:: model(originalClass)
+	Modifies a class such that it becomes part of the model.
 
-	This decorator should be used for any method that changes the data in the
-	model.
-	:param setter: The function to allow registering listeners with.
-	:return: A new function that calls all listeners after calling the setter.
+	This adds signals for each of the class' members, and a ``listenTo`` method.
+	Using the ``listenTo`` method, a different function can be registered to be
+	called every time a certain member changes. This is intended to be used by a
+	viewer to update its view of the model towards the user.
+
+	:param originalClass: The class to turn into a part of the model.
+	:return: The same class, but with added hooks into every of its members that
+	can be listened to.
 	"""
-	global __listeners
-	if not setter in __listeners: #Make an entry to track listeners of this function.
-		__listeners[setter] = WeakSet()
-	@wraps(setter)
-	def newSetter(*args,**kwargs):
-		setter(*args,**kwargs)
-		for listener in __listeners[setter]: #Call all listeners.
+	originalInit = originalClass.__init__
+	@wraps(originalClass.__init__)
+	def newInit(self,*args,**kwargs): #Create a new __init__ function.
+		self.__listeners = {}
+		for member in dir(self): #Add a signal for all static members.
+			self.__listeners[member] = [] #List of functions that need to be called when setting the member
+		originalInit(self,*args,**kwargs)
+	originalClass.__init__ = newInit #Replace the old __init__ with the new one.
+
+	originalSetAttr = originalClass.__setattr__ #Create a new __setattr__ function.
+	@wraps(originalSetAttr)
+	def newSetAttr(self,name,value):
+		originalSetAttr(self,name,value)
+		if name == "__listeners": #Don't want this to be triggered before the listener construction has been set up.
+			return
+		if not name in self.__listeners: #Attribute added dynamically. Add a signal for it.
+			self.__listeners[name] = []
+		for listener in self.__listeners[name]: #Call anything that listens to this member.
 			listener()
-	return newSetter
+	originalClass.__setattr__ = newSetAttr #Replace the old __setattr__ with the new one.
+
+	def listenTo(self,member,function):
+		"""
+		.. function:: listenTo(member,function)
+		Hooks a function to be called whenever the specified member is changed.
+
+		Note that this only works on local members, not on static members.
+
+		:param member: The name of the member to listen to, as string.
+		:param function: The function to call whenever that member is changed.
+		"""
+		if not member in self.__listeners:
+			warning("No member \"{member}\" found to listen to.",member = member)
+			return
+
+		if len(getargspec(function).args) > 1: #Function has arguments. Can't have that.
+			warning("Listener function {function} must not have any arguments.",function = str(function))
+			return
+
+		self.__listeners[member].append(function) #Add this function as listener.
+	originalClass.listenTo = listenTo #Add the listenTo method.
+
+	return originalClass #Return a modified class.
