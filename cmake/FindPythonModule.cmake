5a168bcf569af9f75aca3369d86692ddb2c4cbed
Ghostkeeper <rubend@tutanota.com>
2016-08-10 02:01:14 +0200

    Move find_python_module to separate file
    This keeps the main CMake file cleaner.

---
+++ cmake/FindPythonModule.cmake
@@ -0,0 +1,21 @@
+#Finding/requiring Python modules.
+function(find_python_module module)
+	string(TOUPPER ${module} module_uppercase)
+	if(NOT PYTHON_${module_uppercase})
+		if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+			set(${module}_FIND_REQUIRED TRUE)
+		endif()
+		#Compile a bit of Python that imports the module and checks if that succeeded.
+		execute_process(
+			COMMAND "${PYTHON_EXECUTABLE}" "-c" "import re, ${module}; print(re.compile('/__init__.py.*').sub('', ${module}.__file__))"
+			RESULT_VARIABLE _${module}_status
+			OUTPUT_VARIABLE _${module}_location
+			ERROR_QUIET
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if(NOT _${module}_status)
+			set(PYTHON_${module_uppercase} ${_${module}_location} CACHE FILEPATH "Location of Python module ${module}.")
+		endif(NOT _${module}_status)
+	endif(NOT PYTHON_${module_uppercase})
+	find_package_handle_standard_args(PYTHON_${module} DEFAULT_MSG PYTHON_${module_uppercase})
+endfunction(find_python_module)
