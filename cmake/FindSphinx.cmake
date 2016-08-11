fd612091360bdd80ed2a14ea6175da3b7208671c
Ghostkeeper <rubend@tutanota.com>
2016-08-12 00:41:25 +0200

    Find sphinx-apidoc as well
    This one will gather all docstrings from inside Python for us.

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -2,4 +2,8 @@ find_program(SPHINX_EXECUTABLE NAMES sphinx-build HINTS $ENV{SPHINX_DIR} PATH_SU
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
 
+find_program(SPHINX_APIDOC_EXECUTABLE NAMES sphinx-apidoc HINTS $ENV{SPHINX_DIR} PATH_SUFFIXES bin DOC "Sphinx generator for reStructuredText API files of Python source code.")
+find_package_handle_standard_args(Sphinx-apidoc DEFAULT_MSG SPHINX_APIDOC_EXECUTABLE)
+
 mark_as_advanced(SPHINX_EXECUTABLE)
+mark_as_advanced(SPHINX_APIDOC_EXECUTABLE)
