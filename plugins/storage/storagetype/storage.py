4fb28e3957d257c4fae27e3af91e2cc682bb6024
Ghostkeeper <rubend@tutanota.com>
2016-08-15 00:56:24 +0200

    Add encoding specification to all Python files
    Per PEP-263. We use UTF-8 everywhere.

--- plugins/storage/storagetype/storage.py
+++ plugins/storage/storagetype/storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
 #This software is distributed under the Creative Commons license (CC0) version 1.0. A copy of this license should have been distributed with this software.
 #The license can also be read online: <https://creativecommons.org/publicdomain/zero/1.0/>. If this online license differs from the license provided with this software, the license provided with this software should be applied.
