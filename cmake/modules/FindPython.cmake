18e46614415910ff682ca37c82d990c6ca565f66
Ghostkeeper <rubend@tutanota.com>
2016-09-11 02:08:26 +0200

    Only build Python if not found
    This prevents adding the Python target multiple times. Still execute the library find multiple times though, since it might need to find with different arguments if you run this script multiple times from different places.

--- cmake/modules/FindPython.cmake
+++ cmake/modules/FindPython.cmake
@@ -4,23 +4,25 @@
 option(BUILD_PYTHON "Build the Python dependency from source. If you build this, the newly-built version will get used by Luna. If not, it will search for a pre-installed version on your system." FALSE)
 
 if(BUILD_PYTHON)
-	if(WIN32)
-		message(WARNING "Building Python on Windows is not supported at the moment. You will probably run into problems.")
+	if(NOT PYTHON_FOUND)
+		if(WIN32)
+			message(WARNING "Building Python on Windows is not supported at the moment. You will probably run into problems.")
+		endif()
+		message(STATUS "Building Python from source.")
+		include(ExternalProject)
+		ExternalProject_Add(Python
+			URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+			URL_HASH SHA512=248B3EF2DEFEE7C013E8AC7472B9F2828B1C5B07A2F091EAD46EBDF209BE11DD37911978B590367699D9FAD50F1B98B998BCEEC34FA8369BA30950D3A5FB596F
+			CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads
+			BUILD_IN_SOURCE 1
+		)
+		set(PYTHON_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/python3)
+		set(PYTHON_VERSION_STRING "3.5.2")
+		set(PYTHON_VERSION_MAJOR 3)
+		set(PYTHON_VERSION_MINOR 5)
+		set(PYTHON_VERSION_PATCH 2)
+		set(PYTHON_FOUND TRUE)
 	endif()
-	message(STATUS "Building Python from source.")
-	include(ExternalProject)
-	ExternalProject_Add(Python
-		URL https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-		URL_HASH SHA512=248B3EF2DEFEE7C013E8AC7472B9F2828B1C5B07A2F091EAD46EBDF209BE11DD37911978B590367699D9FAD50F1B98B998BCEEC34FA8369BA30950D3A5FB596F
-		CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-shared --with-threads
-		BUILD_IN_SOURCE 1
-	)
-	set(PYTHON_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/python3)
-	set(PYTHON_VERSION_STRING "3.5.2")
-	set(PYTHON_VERSION_MAJOR 3)
-	set(PYTHON_VERSION_MINOR 5)
-	set(PYTHON_VERSION_PATCH 2)
-	set(PYTHON_FOUND TRUE)
 else() #Just find it on the system.
 	if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED") #Pass the REQUIRED parameter on to the script. Perhaps also do other parameters?
 		find_package(PythonInterp 3.4.0 REQUIRED)
