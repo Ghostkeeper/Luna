b5814e0b66cd360e8aba2ff696c2af45e20ad313
Ruben D <rubend@tutanota.com>
2016-11-05 13:58:52 +0100

    Add empty test case for integration
    This test will test whether the storage plug-ins actually behave as the storage type expects them to.

---
+++ plugins/storage/storagetype/test/test_integration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#This software is distributed under the Creative Commons license (CC0) version 1.0. A copy of this license should have been distributed with this software.
+#The license can also be read online: <https://creativecommons.org/publicdomain/zero/1.0/>. If this online license differs from the license provided with this software, the license provided with this software should be applied.
+
+"""
+Tests for each storage plug-in whether it properly implements the storage
+interface.
+"""
+
+import luna.test_case
+
+class TestIntegration(luna.test_case.TestCase):
+	pass #Not implemented yet.
