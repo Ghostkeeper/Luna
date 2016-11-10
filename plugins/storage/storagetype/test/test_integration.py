1a859c213f94f49d1c8f140a7a22c28dc5fac6f0
Ghostkeeper <rubend@tutanota.com>
2016-11-11 00:48:55 +0100

    Add tests for can_write
    These will check if the can_write functions of each storage plug-in behave as expected.

--- plugins/storage/storagetype/test/test_integration.py
+++ plugins/storage/storagetype/test/test_integration.py
@@ -34,6 +34,22 @@ for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
 	can_read_exception_parameters["number_" + identity] = {"can_read": can_read_function, "uri": 42,   "exception": Exception}
 	can_read_exception_parameters["none_" + identity] =   {"can_read": can_read_function, "uri": None, "exception": Exception}
 
+can_write_parameters = {}
+for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
+	can_write_function = metadata["storage"]["can_write"]
+	can_write_parameters["abs_path_" + identity] =     {"can_write": can_write_function, "uri": "file:/path/to/file.ext"}
+	can_write_parameters["full_path_" + identity] =    {"can_write": can_write_function, "uri": "frag://auth/path/path/file.ext?query=value#fragment"}
+	can_write_parameters["relative_" + identity] =     {"can_write": can_write_function, "uri": "relative/path/file.ext"}
+	can_write_parameters["backslash_" + identity] =    {"can_write": can_write_function, "uri": "\\\\path\\with\\backslashes"}
+	can_write_parameters["invalid_auth_" + identity] = {"can_write": can_write_function, "uri": "https://[invalid_auth/file.ext"} #urllib.parse.parseurl gives a ValueError here.
+	can_write_parameters["empty_" + identity] =        {"can_write": can_write_function, "uri": ""}
+
+can_write_exception_parameters = {}
+for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
+	can_write_function = metadata["storage"]["can_write"]
+	can_write_exception_parameters["number_" + identity] = {"can_write": can_write_function, "uri": 42,   "exception": Exception}
+	can_write_exception_parameters["none_" + identity]   = {"can_write": can_write_function, "uri": None, "exception": Exception}
+
 class TestIntegration(luna.test_case.TestCase):
 	"""
 	Tests for each storage plug-in whether it properly implements the storage
@@ -65,3 +81,29 @@ class TestIntegration(luna.test_case.TestCase):
 		"""
 		with self.assertRaises(exception):
 			can_read(uri)
+
+	@luna.test_case.parametrise(can_write_parameters)
+	def test_can_write(self, can_write, uri):
+		"""
+		Tests the can_write function with normal parameters.
+
+		The can_write function must always return a boolean answer in that case.
+
+		:param can_write: The can_write function to test.
+		:param uri: The URI to feed to the can_write function.
+		"""
+		self.assertIsInstance(can_write(uri), bool) #These must always give True or False as answer.
+
+	@luna.test_case.parametrise(can_write_exception_parameters)
+	def test_can_write_exception(self, can_write, uri, exception):
+		"""
+		Tests the can_write function with faulty parameters.
+
+		The can_write function must raise an exception in that case.
+
+		:param can_write: The can_write function to test.
+		:param uri: The URI to feed to the can_write function.
+		:param exception: The exception the function is expected to raise.
+		"""
+		with self.assertRaises(exception):
+			can_write(uri)
