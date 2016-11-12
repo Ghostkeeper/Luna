9eb75e7690fbbd8e1a25e5627cab61ae114906c7
Ghostkeeper <rubend@tutanota.com>
2016-11-12 15:34:24 +0100

    Make dynamic function parameters private
    Only this test case should use them, really.

--- plugins/storage/storagetype/test/test_integration.py
+++ plugins/storage/storagetype/test/test_integration.py
@@ -21,37 +21,37 @@ luna.plugins.discover()
 #pylint: disable=C0326
 
 #Dynamically prepare the parameters for the parametrised tests.
-can_read_parameters = {}
+_can_read_parameters = {}
 for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
 	can_read_function = metadata["storage"]["can_read"]
-	can_read_parameters["abs_path_" + identity] =     {"can_read": can_read_function, "uri": "file:/path/to/file.ext"}
-	can_read_parameters["full_path_" + identity] =    {"can_read": can_read_function, "uri": "frag://auth/path/path/file.ext?query=value#fragment"}
-	can_read_parameters["relative_" + identity] =     {"can_read": can_read_function, "uri": "relative/path/file.ext"}
-	can_read_parameters["backslash_" + identity] =    {"can_read": can_read_function, "uri": "\\\\path\\with\\backslashes"}
-	can_read_parameters["invalid_auth_" + identity] = {"can_read": can_read_function, "uri": "https://[invalid_auth/file.ext"} #urllib.parse.parseurl gives a ValueError here.
-	can_read_parameters["empty_" + identity] =        {"can_read": can_read_function, "uri": ""}
+	_can_read_parameters["abs_path_" + identity] =     { "can_read": can_read_function, "uri": "file:/path/to/file.ext" }
+	_can_read_parameters["full_path_" + identity] =    { "can_read": can_read_function, "uri": "frag://auth/path/path/file.ext?query=value#fragment" }
+	_can_read_parameters["relative_" + identity] =     { "can_read": can_read_function, "uri": "relative/path/file.ext" }
+	_can_read_parameters["backslash_" + identity] =    { "can_read": can_read_function, "uri": "\\\\path\\with\\backslashes" }
+	_can_read_parameters["invalid_auth_" + identity] = { "can_read": can_read_function, "uri": "https://[invalid_auth/file.ext" } #urllib.parse.parseurl gives a ValueError here.
+	_can_read_parameters["empty_" + identity] =        { "can_read": can_read_function, "uri": "" }
 
-can_read_exception_parameters = {}
+_can_read_exception_parameters = {}
 for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
 	can_read_function = metadata["storage"]["can_read"]
-	can_read_exception_parameters["number_" + identity] = {"can_read": can_read_function, "uri": 42,   "exception": Exception}
-	can_read_exception_parameters["none_" + identity] =   {"can_read": can_read_function, "uri": None, "exception": Exception}
+	_can_read_exception_parameters["number_" + identity] = { "can_read": can_read_function, "uri": 42, "exception": Exception }
+	_can_read_exception_parameters["none_" + identity] =   { "can_read": can_read_function, "uri": None, "exception": Exception }
 
-can_write_parameters = {}
+_can_write_parameters = {}
 for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
 	can_write_function = metadata["storage"]["can_write"]
-	can_write_parameters["abs_path_" + identity] =     {"can_write": can_write_function, "uri": "file:/path/to/file.ext"}
-	can_write_parameters["full_path_" + identity] =    {"can_write": can_write_function, "uri": "frag://auth/path/path/file.ext?query=value#fragment"}
-	can_write_parameters["relative_" + identity] =     {"can_write": can_write_function, "uri": "relative/path/file.ext"}
-	can_write_parameters["backslash_" + identity] =    {"can_write": can_write_function, "uri": "\\\\path\\with\\backslashes"}
-	can_write_parameters["invalid_auth_" + identity] = {"can_write": can_write_function, "uri": "https://[invalid_auth/file.ext"} #urllib.parse.parseurl gives a ValueError here.
-	can_write_parameters["empty_" + identity] =        {"can_write": can_write_function, "uri": ""}
+	_can_write_parameters["abs_path_" + identity] =     { "can_write": can_write_function, "uri": "file:/path/to/file.ext" }
+	_can_write_parameters["full_path_" + identity] =    { "can_write": can_write_function, "uri": "frag://auth/path/path/file.ext?query=value#fragment" }
+	_can_write_parameters["relative_" + identity] =     { "can_write": can_write_function, "uri": "relative/path/file.ext" }
+	_can_write_parameters["backslash_" + identity] =    { "can_write": can_write_function, "uri": "\\\\path\\with\\backslashes" }
+	_can_write_parameters["invalid_auth_" + identity] = { "can_write": can_write_function, "uri": "https://[invalid_auth/file.ext" } #urllib.parse.parseurl gives a ValueError here.
+	_can_write_parameters["empty_" + identity] =        { "can_write": can_write_function, "uri": "" }
 
-can_write_exception_parameters = {}
+_can_write_exception_parameters = {}
 for identity, metadata in luna.plugins.plugins_by_type["storage"].items():
 	can_write_function = metadata["storage"]["can_write"]
-	can_write_exception_parameters["number_" + identity] = {"can_write": can_write_function, "uri": 42,   "exception": Exception}
-	can_write_exception_parameters["none_" + identity]   = {"can_write": can_write_function, "uri": None, "exception": Exception}
+	_can_write_exception_parameters["number_" + identity] = { "can_write": can_write_function, "uri": 42, "exception": Exception }
+	_can_write_exception_parameters["none_" + identity]   = { "can_write": can_write_function, "uri": None, "exception": Exception }
 
 class TestIntegration(luna.test_case.TestCase):
 	"""
@@ -59,7 +59,7 @@ class TestIntegration(luna.test_case.TestCase):
 	interface.
 	"""
 
-	@luna.test_case.parametrise(can_read_parameters)
+	@luna.test_case.parametrise(_can_read_parameters)
 	def test_can_read(self, can_read, uri):
 		"""
 		Tests the can_read function with normal parameters.
@@ -71,7 +71,7 @@ class TestIntegration(luna.test_case.TestCase):
 		"""
 		self.assertIsInstance(can_read(uri), bool) #These must always give True or False as answer.
 
-	@luna.test_case.parametrise(can_read_exception_parameters)
+	@luna.test_case.parametrise(_can_read_exception_parameters)
 	def test_can_read_exception(self, can_read, uri, exception):
 		"""
 		Tests the can_read function with faulty parameters.
@@ -85,7 +85,7 @@ class TestIntegration(luna.test_case.TestCase):
 		with self.assertRaises(exception):
 			can_read(uri)
 
-	@luna.test_case.parametrise(can_write_parameters)
+	@luna.test_case.parametrise(_can_write_parameters)
 	def test_can_write(self, can_write, uri):
 		"""
 		Tests the can_write function with normal parameters.
@@ -97,7 +97,7 @@ class TestIntegration(luna.test_case.TestCase):
 		"""
 		self.assertIsInstance(can_write(uri), bool) #These must always give True or False as answer.
 
-	@luna.test_case.parametrise(can_write_exception_parameters)
+	@luna.test_case.parametrise(_can_write_exception_parameters)
 	def test_can_write_exception(self, can_write, uri, exception):
 		"""
 		Tests the can_write function with faulty parameters.
