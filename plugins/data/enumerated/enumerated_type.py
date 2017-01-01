a3038f45648f1a6ac9d8cfb9e18d79a33b9d70b9
Ghostkeeper <rubend@tutanota.com>
2017-01-01 14:35:17 +0100

    Implement serialisation properly
    The is_serialised check has a rather expansive check for allowed characters, but it's the only way I could implement it with a possibility for lazy evaluation in the future. Only it has no lazy evaluation right now since it needs to decode all characters first...

--- plugins/data/enumerated/enumerated_type.py
+++ plugins/data/enumerated/enumerated_type.py
@@ -9,7 +9,8 @@ Provides the implementation of enumerated types as a data type definition.
 """
 
 import enum #To check types against the Enum class.
-import pickle #To serialise Enums.
+import sys #To find pre-loaded enums in their original modules.
+import unicodedata #To see if the serialisation of enums has only allowed characters.
 
 def serialise(instance):
 	"""
@@ -18,7 +19,11 @@ def serialise(instance):
 	:param instance: The instance of an enumerated type.
 	:return: A byte sequence representing the enumerated type.
 	"""
-	return pickle.dumps(instance, protocol=pickle.HIGHEST_PROTOCOL) #Only need to be compatible with this same version of Python, so always use the highest protocol.
+	try:
+		reference = instance.__module__ + "." + instance.__class__.__name__ + "." + e.name
+	except TypeError: #Translate the cryptic type error that arises from this if it is no enum.
+		raise TypeError("Trying to serialise something that is not an enumerated type: {instance}".format(instance=str(instance)))
+	return reference.encode(encoding="utf_8")
 
 def deserialise(serialised):
 	"""
@@ -27,7 +32,13 @@ def deserialise(serialised):
 	:param serialised: A sequence of bytes that represents an enumerated type.
 	:return: An instance of the enumerated type the sequence represents.
 	"""
-	return pickle.loads(serialised)
+	serialised_string = serialised.decode(encoding="utf_8")
+	path = serialised_string.split(".")
+	module_string = ".".join(path[0:-2]) #Remove everything after the last two dots.
+	module = sys.modules[module_string] #The module may not yet be imported at this point, but if it isn't then the plug-in is not loaded and should be considered unsafe.
+	enum_class = getattr(module, path[-2]) #Enum must be reachable from the top level of the module!
+	enum_instance = getattr(enum_class, path[-1])
+	return enum_instance
 
 def is_instance(instance):
 	"""
@@ -48,4 +59,57 @@ def is_serialised(serialised):
 	:return: ``True`` if the sequence represents an enumerated type, or
 	``False`` if it doesn't.
 	"""
-	raise NotImplementedError("The is_serialised function is not yet implemented.")
+	unicode_string = serialised.decode(encoding="utf_8") #TODO: Lazy decode?
+	next_should_continue = False #Whether the next character should be a continuation character (True) or a start character (False)
+	for character in unicode_string:
+		if next_should_continue: #Should be a continuation character.
+			if not _is_id_continue(character) and character != ".": #Also allow periods since Enums are serialised as fully qualified names.
+				return False
+			if character == ".":
+				next_should_continue = False
+		else: #Should be a start character.
+			if not _is_id_start(character):
+				return False
+			next_should_continue = True
+
+_allowed_id_continue_categories = {"Ll", "Lm", "Lo", "Lt", "Lu", "Mc", "Mn", "Nd", "Nl", "Pc"}
+"""
+Character categories that Python identifiers are allowed to continue with.
+"""
+
+_allowed_id_continue_characters = {"_", "\u00B7", "\u0387", "\u1369", "\u136A", "\u136B", "\u136C", "\u136D", "\u136E", "\u136F", "\u1370", "\u1371", "\u19DA", "\u2118", "\u212E", "\u309B", "\u309C"}
+"""
+Additional characters that Python identifiers are allowed to continue with.
+"""
+
+_allowed_id_start_categories = {"Ll", "Lm", "Lo", "Lt", "Lu", "Nl"}
+"""
+Character categories that Python identifiers are allowed to start with.
+"""
+
+_allowed_id_start_characters = {"_", "\u2118", "\u212E", "\u309B", "\u309C"}
+"""
+Additional characters that Python identifiers are allowed to start with.
+"""
+
+def _is_id_start(character):
+	"""
+	Returns whether a character is an allowed first character of an identifier
+	in Python.
+
+	:param character: The character to check for.
+	:return: ``True`` if the character is allowed as a first character, or
+	``False`` if it isn't.
+	"""
+	return unicodedata.category(character) in _allowed_id_start_categories or character in _allowed_id_start_categories or unicodedata.category(unicodedata.normalize("NFKC", character)) in _allowed_id_start_categories or unicodedata.normalize("NFKC", character) in _allowed_id_start_characters
+
+def _is_id_continue(character):
+	"""
+	Returns whether a character is an allowed continuation character of an
+	identifier in Python.
+
+	:param character: The character to check for.
+	:return: ``True`` if the character is allowed as a first character, or
+	``False`` if it isn't.
+	"""
+	return unicodedata.category(character) in _allowed_id_continue_categories or character in _allowed_id_continue_characters or unicodedata.category(unicodedata.normalize("NFKC", character)) in _allowed_id_continue_categories or unicodedata.normalize("NFKC", character) in _allowed_id_continue_characters
