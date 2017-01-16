a85a2911f1bfb3d87c19cf3b4daeb0f6c11c15d5
Ruben D <rubend@tutanota.com>
2017-01-16 01:07:25 +0100

    Add stub for preferences plug-in
    This plug-in allows making global application preferences, such as language or window behaviour or something.

---
+++ plugins/configuration/preferences/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#This software is distributed under the Creative Commons license (CC0) version 1.0. A copy of this license should have been distributed with this software.
+#The license can also be read online: <https://creativecommons.org/publicdomain/zero/1.0/>. If this online license differs from the license provided with this software, the license provided with this software should be applied.
+
+"""
+Provides global application preferences.
+
+This allows the user to personalise the general workings and look of the
+application to his liking. Other components can register preferences with this
+plug-in in order to preserve the state of a variable over multiple runs of the
+application.
+"""
+
+import preferences.preferences as preferences_module #The module containing the class that implements the preferences.
+
+def metadata():
+	"""
+	Provides the metadata for the Preferences plug-in.
+
+	This gives human-readable information on the plug-in, dependency resolution
+	information, and tells the plug-in system what this plug-in can do.
+	:return: Dictionary of metadata.
+	"""
+	return {
+		"name": "Preferences",
+		"description": "Allows storing and modifying general application preferences.",
+		"version": 1,
+		"dependencies": {
+			"configurationtype": {
+				"version_min": 1,
+				"version_max": 1
+			},
+			"datatype": {
+				"version_min": 1,
+				"version_max": 1
+			}
+		},
+
+		"configuration": {
+			"name": "Preferences",
+			"instance": preferences_module.Preferences()
+		}
+	}
---
+++ plugins/configuration/preferences/preferences.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#This software is distributed under the Creative Commons license (CC0) version 1.0. A copy of this license should have been distributed with this software.
+#The license can also be read online: <https://creativecommons.org/publicdomain/zero/1.0/>. If this online license differs from the license provided with this software, the license provided with this software should be applied.
+
+import luna.plugins #To get the configuration data type to extend from.
+
+class Preferences:
+	pass #TODO: We must somehow extend from a class in a different module, but at definition time that module may not have loaded yet.
